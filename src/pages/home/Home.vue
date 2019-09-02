cda0db294c7f8b6bb036d822d378f6f656499fd8
Matiko <matiko@Matiko.local>
2019-09-02 17:24:55 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    hello world
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+  import HomeHeader from './components/Header'
+  export default {
+    name: 'Home',
+    comments:{
+      HomeHeader
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  this is home header!!!
+</div>
+</template>
+
+<script>
+
+  export default {
+    name: 'HomeHeader'
+  }
+</script>
+
+<style scoped>
+
+</style>
