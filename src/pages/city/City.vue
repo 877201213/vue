8a07d96be6c0c06ec0e6a8c7d1bcd9bbc151cffc
Matiko <877201213@qq.com>
2019-09-20 13:53:04 +0800

    finish ctiy css

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,19 +2,20 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
 </div>
 </template>
 
 <script>
 import CityHeader from '@/pages/city/components/Header'
 import CitySearch from '@/pages/city/components/Search'
+import CityList from '@/pages/city/components/List'
+import CityAlphabet from '@/pages/city/components/Alphabet'
+
 export default {
   name: 'City',
-  components: {CitySearch, CityHeader},
-  comments: {
-    CityHeader,
-    CitySearch
-  }
+  components: {CityAlphabet, CityList, CitySearch, CityHeader}
 }
 </script>
 
