7faa6093292afaed9117776051a3e663fba8a04a
Matiko <877201213@qq.com>
2019-09-20 15:11:10 +0800

    ajax city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities = 'cities' :hot = 'hotCities'></city-list>
+    <city-alphabet :cities ='cities'></city-alphabet>
 </div>
 </template>
 
@@ -12,10 +12,34 @@ import CityHeader from '@/pages/city/components/Header'
 import CitySearch from '@/pages/city/components/Search'
 import CityList from '@/pages/city/components/List'
 import CityAlphabet from '@/pages/city/components/Alphabet'
+import axios from 'axios'
 
 export default {
   name: 'City',
-  components: {CityAlphabet, CityList, CitySearch, CityHeader}
+  components: {CityAlphabet, CityList, CitySearch, CityHeader},
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  }
 }
 </script>
 
