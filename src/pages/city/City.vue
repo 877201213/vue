42ec501c1538e4502eeb88218886d90fc221c46b
Matiko <877201213@qq.com>
2019-09-20 16:01:57 +0800

    perfomance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,15 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities = 'cities' :hot = 'hotCities'></city-list>
-    <city-alphabet :cities ='cities'></city-alphabet>
+    <city-list
+      :cities = 'cities'
+      :hot = 'hotCities'
+      :letter = 'letter'
+    ></city-list>
+    <city-alphabet
+      :cities ='cities'
+      @change="handleLetterChang"
+    ></city-alphabet>
 </div>
 </template>
 
@@ -20,7 +27,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -35,6 +43,9 @@ export default {
         this.cities = data.cities
         this.hotCities = data.hotCities
       }
+    },
+    handleLetterChang (letter) {
+      this.letter = letter
     }
   },
   mounted () {
